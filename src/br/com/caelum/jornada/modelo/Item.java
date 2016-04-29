fef6791d9574e8e1c0831b49f4302eb3b99afce1
Igor Galvão <igorgbueno@gmail.com>
2016-04-29 16:07:07 -0300

    Correção de problema na adição de itens iguais ao carrinho

--- src/br/com/caelum/jornada/modelo/Item.java
+++ src/br/com/caelum/jornada/modelo/Item.java
@@ -26,5 +26,38 @@ public class Item {
 	public void setPreco(Preco preco) {
 		this.preco = preco;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((livro == null) ? 0 : livro.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (livro == null) {
+			if (other.livro != null)
+				return false;
+		} else if (!livro.equals(other.livro))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
--- src/br/com/caelum/jornada/modelo/Preco.java
+++ src/br/com/caelum/jornada/modelo/Preco.java
@@ -27,4 +27,25 @@ public class Preco {
 		this.valor = valor;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((tipoLivro == null) ? 0 : tipoLivro.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Preco other = (Preco) obj;
+		if (tipoLivro != other.tipoLivro)
+			return false;
+		return true;
+	}	
+
 }
